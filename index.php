aa29e33af928abd2b978c5246cd894188d916d14
Ryan Weaver <ryan@thatsquality.com>
2011-12-01 19:27:46 -0600

    Refactoring to use a response object

--- index.php
+++ index.php
@@ -10,30 +10,39 @@ try {
 
 // create a request object to help us
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 $request = Request::createFromGlobals();
 
 $uri = $request->getPathInfo();
 
 if ($uri == '/' || $uri == '') {
 
-    echo '<h1>Welcome to PHP Santa</h1>';
-    echo sprintf('<a href="/letters">Read the letters</a>');
+    $content = '<h1>Welcome to PHP Santa</h1>';
+    $content .= sprintf('<a href="/letters">Read the letters</a>');
     if ($name = $request->query->get('name')) {
-        echo sprintf('<p>Oh, and hello %s!</p>', $_GET['name']);
+        $content .= sprintf('<p>Oh, and hello %s!</p>', $_GET['name']);
     }
+
+    $response = new Response($content);
 
 } elseif ($uri == '/letters') {
 
     $sql = 'SELECT * FROM php_santa_letters';
-    echo '<h1>Read the letters to PHP Santa</h1>';
-    echo '<ul>';
+    $content = '<h1>Read the letters to PHP Santa</h1>';
+    $content .= '<ul>';
     foreach ($dbh->query($sql) as $row) {
-        echo sprintf('<li>%s - dated %s</li>', $row['content'], $row['received_at']);
+        $content .= sprintf('<li>%s - dated %s</li>', $row['content'], $row['received_at']);
     }
-    echo '</ul>';
+    $content .= '</ul>';
+
+    $response = new Response($content);
 
 } else {
-    header("HTTP/1.1 404 Not Found");
-    echo '<h1>404 Page not Found</h1>';
-    echo '<p>This is most certainly *not* an xmas miracle</p>';
+    $content = '<h1>404 Page not Found</h1>';
+    $content .= '<p>This is most certainly *not* an xmas miracle</p>';
+
+    $response = new Response($content);
+    $response->setStatusCode(404);
 }
+
+$response->send();
