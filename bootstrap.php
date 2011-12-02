9faf266d7020d0f50f2a6183b9ec94c20583707c
Ryan Weaver <ryan@thatsquality.com>
2011-12-01 23:08:22 -0600

    Shortening class name

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,6 @@ require __DIR__.'/vendors/Symfony/Component/ClassLoader/UniversalClassLoader.php
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 // setup the autoloader
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+$loader = new UniversalClassLoader();
 $loader->registerNamespace('Symfony', __DIR__.'/vendors');
 $loader->register();
