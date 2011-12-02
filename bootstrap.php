03e4876c62131a9579bf92a75436534a5b6b56aa
Ryan Weaver <ryan@thatsquality.com>
2011-12-01 19:59:57 -0600

    Refactoring hardcore to use Lithium's router

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,6 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 // setup the autoloader
 $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespace('Symfony', __DIR__.'/vendors');
+$loader->registerNamespace('lithium', __DIR__.'/vendors');
+
 $loader->register();
--- index.php
+++ index.php
@@ -11,21 +11,53 @@ try {
 // create a request object to help us
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use lithium\net\http\Router;
+use lithium\action\Request as Li3Request;
+
 $request = Request::createFromGlobals();
 
-$uri = $request->getPathInfo();
+// create a lithium request from a Symfony request, since the interfaces aren't compatible
+$li3Request = new Li3Request();
+$li3Request->url = $request->getPathInfo();
 
-if ($uri == '/' || $uri == '') {
+// create a router, build the routes, and then execute it
+$router = new \lithium\net\http\Router();
+$router->connect('/', array('controller' => 'homepage'));
+$router->connect('/letters', array('controller' => 'letters'));
+$li3Request = $router->parse($li3Request);
 
+// grab the controller from the results
+if ($li3Request && isset($li3Request->params['controller'])) {
+    $controller = $li3Request->params['controller'];
+} else {
+    $controller = 'error404';
+}
+
+// execute the controller and get the response
+$response = call_user_func_array($controller, array($request));
+if (!$response instanceof Response) {
+    throw new Exception(sprintf('Your controller "%s" did not return a response!!', $controller));
+}
+
+$response->send();
+
+/**
+ * My Controllers!!!!
+ */
+
+function homepage(Request $request) {
     $content = '<h1>Welcome to PHP Santa</h1>';
     $content .= sprintf('<a href="/letters">Read the letters</a>');
     if ($name = $request->query->get('name')) {
         $content .= sprintf('<p>Oh, and hello %s!</p>', $name);
     }
 
-    $response = new Response($content);
+    return new Response($content);
+}
 
-} elseif ($uri == '/letters') {
+function letters(Request $request)
+{
+    global $dbh;
 
     $sql = 'SELECT * FROM php_santa_letters';
     $content = '<h1>Read the letters to PHP Santa</h1>';
@@ -35,14 +67,16 @@ if ($uri == '/' || $uri == '') {
     }
     $content .= '</ul>';
 
-    $response = new Response($content);
+    return new Response($content);
+}
 
-} else {
+function error404(Request $request)
+{
     $content = '<h1>404 Page not Found</h1>';
     $content .= '<p>This is most certainly *not* an xmas miracle</p>';
 
     $response = new Response($content);
     $response->setStatusCode(404);
-}
 
-$response->send();
+    return $response;
+}
